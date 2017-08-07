fbd28b0454503b50772d6ae8a7980e0a24fb52f7
Michael Sumner <mdsumner@gmail.com>
2017-08-07 06:34:52 +0000

    first pass at yearly files

--- data-raw/sokolov-rintoul.R
+++ data-raw/sokolov-rintoul.R
@@ -10,9 +10,9 @@ d <- d %>% rename(x_ = X1, y_ = X2, object_ = filename, branch_ = X3) %>%
   mutate(order_ = row_number())
 library(spbabel)
 srfronts <- sp(d, attr_tab = d %>% transmute(front = object_) %>% distinct(), crs = "+init=epsg:4326")
-plot(srfronts, col = bpy.colors(nrow(srfronts)))
-library(maps)
-map("world2", add = TRUE)
+#plot(srfronts, col = bpy.colors(nrow(srfronts)))
+#library(maps)
+#map("world2", add = TRUE)
 
 devtools::use_data(srfronts)
 #devtools::install_github("tidyverse/ggplot2")
@@ -21,3 +21,15 @@ devtools::use_data(srfronts)
 # ggplot(st_as_sf(srfronts)) + 
 #   geom_sf(aes(colour = front)) + 
 #   coord_sf( crs = "+proj=stere +lat_0=-90 +datum=WGS84")
+
+
+
+yfiles <- tibble::tibble(fullname = list.files("data-raw/sokolov-rintoul-source/yearly", full.names = TRUE, pattern = "mat$"))
+x <- R.matlab::readMat(yfiles$fullname[1])
+## this loses the object level grouping so need to unpick that a bit more
+lon <- dplyr::bind_rows(unlist(rapply(x$Fronts[[1]], classes = "matrix", f = function(y) tibble::tibble(x = as.vector(y)), how = "replace"), recursive = FALSE), .id = "blah")
+lat <- dplyr::bind_rows(unlist(rapply(x$Fronts[[2]], classes = "matrix", f = function(y) tibble::tibble(y = as.vector(y)), how = "replace"), recursive = FALSE), .id = "blah")
+
+d <- bind_cols(lon, lat %>% select(-blah))
+ggplot(d, aes(x, y, group = blah, colour  = blah)) + geom_path() + guides(colour = FALSE)
+
